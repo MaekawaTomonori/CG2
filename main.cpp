b5ba919266d35287e7bdb23d9d625dc6b4f78684
Apocalypse <k023g0063@g.neec.ac.jp>
2024-04-17 15:20:50 +0900

    DirectXの初期化 コマンドリストの作成

--- main.cpp
+++ main.cpp
@@ -1,5 +1,14 @@
 #include <Windows.h>
 #include <cstdint>
+#include <format>
+#include <string>
+#include <d3d12.h>
+#include <dxgi1_6.h>
+#include <cassert>
+
+#pragma comment(lib, "d3d12.lib")
+#pragma comment(lib, "dxgi.lib")
+
 
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
     switch (msg){
@@ -11,20 +20,57 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
     return DefWindowProc(hwnd, msg, wParam, lParam);
 }
 
+std::wstring ConvertString(const std::string& str) {
+    if (str.empty()){
+        return std::wstring();
+    }
+
+    auto sizeNeeded = MultiByteToWideChar(CP_UTF8, 0, reinterpret_cast<const char*>(&str[0]), static_cast<int>(str.size()), NULL, 0);
+    if (sizeNeeded == 0){
+        return std::wstring();
+    }
+    std::wstring result(sizeNeeded, 0);
+    MultiByteToWideChar(CP_UTF8, 0, reinterpret_cast<const char*>(&str[0]), static_cast<int>(str.size()), &result[0], sizeNeeded);
+    return result;
+}
+
+std::string ConvertString(const std::wstring& str) {
+    if (str.empty()){
+        return std::string();
+    }
+
+    auto sizeNeeded = WideCharToMultiByte(CP_UTF8, 0, str.data(), static_cast<int>(str.size()), NULL, 0, NULL, NULL);
+    if (sizeNeeded == 0){
+        return std::string();
+    }
+    std::string result(sizeNeeded, 0);
+    WideCharToMultiByte(CP_UTF8, 0, str.data(), static_cast<int>(str.size()), result.data(), sizeNeeded, NULL, NULL);
+    return result;
+}
+
+void Log(const std::string& message) {
+    OutputDebugStringA(message.c_str());
+}
+
+void Log(const std::wstring& message) {
+    OutputDebugStringA(ConvertString(message).c_str());
+}
+
+//Rect
+const int32_t CLIENT_WIDTH = 1280;
+const int32_t CLIENT_HEIGHT = 720;
+
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
     //Registering Window Class
     WNDCLASS wc {};
 
     wc.lpfnWndProc = WindowProc;
-    wc.lpszClassName = L"CG2WindowClass";
+    wc.lpszClassName = L"WindowClass";
     wc.hInstance = GetModuleHandle(nullptr);
     wc.hCursor = LoadCursor(nullptr, IDC_ARROW);
 
     RegisterClass(&wc);
 
-    //Rect
-    const int32_t CLIENT_WIDTH = 1280;
-    const int32_t CLIENT_HEIGHT = 720;
 
     RECT wrc = {0,0,CLIENT_WIDTH,CLIENT_HEIGHT};
 
@@ -46,6 +92,80 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
     );
 
     ShowWindow(hwnd_, SW_SHOW);
+
+    //=================================
+
+    //Initialize DirectX and Registering GPU
+
+    IDXGIFactory7* dxgiFactory = nullptr;
+    HRESULT hResult = CreateDXGIFactory(IID_PPV_ARGS(&dxgiFactory));
+
+    assert(SUCCEEDED(hResult));
+
+    IDXGIAdapter4* useAdapter = nullptr;
+    for (UINT i = 0; dxgiFactory->EnumAdapterByGpuPreference(i, DXGI_GPU_PREFERENCE_HIGH_PERFORMANCE, IID_PPV_ARGS(&useAdapter)) != DXGI_ERROR_NOT_FOUND; ++i){
+        DXGI_ADAPTER_DESC3 adapterDesc {};
+        hResult = useAdapter->GetDesc3(&adapterDesc);
+        assert(SUCCEEDED(hResult));
+
+        if (!(adapterDesc.Flags & DXGI_ADAPTER_FLAG3_SOFTWARE)){
+            Log(std::format(L"Use Adapter:{}\n", adapterDesc.Description));
+            break;
+        }
+        useAdapter = nullptr;
+    }
+
+    assert(useAdapter != nullptr);
+
+    ID3D12Device* device = nullptr;
+
+    D3D_FEATURE_LEVEL featureLevels[] = {
+        D3D_FEATURE_LEVEL_12_2, D3D_FEATURE_LEVEL_12_1,D3D_FEATURE_LEVEL_12_0
+    };
+    const char* featureLevelStrings[] = {"12.2", "12.1", "12.0"};
+
+    for (size_t i = 0; i < _countof(featureLevels); ++i){
+        hResult = D3D12CreateDevice(useAdapter, featureLevels[i], IID_PPV_ARGS(&device));
+
+        if (SUCCEEDED(hResult)){
+            Log(std::format("FeatureLevel : {}\n", featureLevelStrings[i]));
+            break;
+        }
+    }
+
+    assert(device != nullptr);
+    Log(ConvertString(std::format(L"Complete create D3D12Device!!!\n")));
+
+    //================================================================================
+
+    //Create CommandQueue
+    ID3D12CommandQueue* commandQueue = nullptr;
+    D3D12_COMMAND_QUEUE_DESC commandQueueDesc {};
+    hResult = device->CreateCommandQueue(&commandQueueDesc, IID_PPV_ARGS(&commandQueue));
+
+    assert(SUCCEEDED(hResult));
+
+    //Create CommandList
+
+    //Command Alloc
+    ID3D12CommandAllocator* commandAllocator = nullptr;
+    hResult = device->CreateCommandAllocator(D3D12_COMMAND_LIST_TYPE_DIRECT, IID_PPV_ARGS(&commandAllocator));
+    assert(SUCCEEDED(hResult));
+
+    //Command List
+    ID3D12GraphicsCommandList* commandList = nullptr;
+    hResult = device->CreateCommandList(0, D3D12_COMMAND_LIST_TYPE_DIRECT, commandAllocator, nullptr, IID_PPV_ARGS(&commandList));
+    assert(SUCCEEDED(hResult));
+
+    MSG msg {};
+    while(msg.message != WM_QUIT){
+        if(PeekMessage(&msg, NULL, 0, 0, PM_REMOVE)){
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }else{
+            //do somethings...//
+        }
+    }
 
     return 0;
 }
