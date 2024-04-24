8a0aba11323c2507165c5a289fd7a9de6c3c1657
Apocalypse <k023g0063@g.neec.ac.jp>
2024-04-24 14:38:25 +0900

    01_03 DirectXの基本的な設定

--- Pch.h
+++ Pch.h
@@ -4,9 +4,11 @@
 #include <string>
 #include <d3d12.h>
 #include <dxgi1_6.h>
+#include <dxgidebug.h>
 #include <cassert>
 
 #pragma comment(lib, "d3d12.lib")
 #pragma comment(lib, "dxgi.lib")
+#pragma comment(lib, "dxguid.lib")
 
 #include "System.h"
